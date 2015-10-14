1a561514e7bdab6bfac8f9c25eb5088ca2a6c338
egohub <ego@hackermail.com>
2015-10-14 22:13:45 +0630

    Update config.rb

--- config.rb
+++ config.rb
@@ -2,12 +2,12 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "js"
-fonts_dir = "fonts"
+http_path = ""
+css_dir = ""
+sass_dir = ""
+images_dir = ""
+javascripts_dir = ""
+fonts_dir = ""
 
 output_style = :expanded
 
